07c7e8883890609a8b720370acc2f5b74e6136fc
Ishwar Lal <138602811+ishwar0980@users.noreply.github.com>
2025-07-02 19:15:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'webxzonebot'
+    return 'Channel Request Accept Bot'
 if __name__ == "__main__":
     app.run()
